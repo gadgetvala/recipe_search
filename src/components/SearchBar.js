240eed72dd43ddf8b1c8d3805c1813e676bd0b53
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-12-09 17:52:20 +0530

    clear searchbar and alerts

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,52 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import "./css/SearchBar.css";
 
 const SearchBar = props => {
 
+
 	//Using State
-	const [term,setTerm] = useState("");
+	const [term, setTerm] = useState("");
 
 	//For Updateing searchterm in state
 	const onChangeTerm = e => {
-    	setTerm(e.target.value);
-  	};
+		setTerm(e.target.value);
+	};
 
-  	//When we submit our form
-  	const onSubmit = e => {
-    	e.preventDefault();
-    	props.setSearch(term);
- 	};
+	//When we submit our form
+	const onSubmit = e => {
+		e.preventDefault();
+		if (term === '') {
+			props.showAlert('First enter the details', 'danger');
+		} else {
+			props.showAlert('Items Searching...', 'success');
+		}
+		props.setSearch(term);
+	};
+
+	// Clear Search input text
+	const clearText = () => {
+		if (term === '') {
+			props.showAlert('Nothing to erase', 'info');
+		} else {
+			setTerm("")
+			props.showAlert('Values erased successfully', 'success');
+		}
+
+		document.getElementById('search-value').focus();
+	};
 
 	return (
-		<form onSubmit={onSubmit} className="search-form">
-	        <input className="search-bar" value={term} onChange={onChangeTerm}/>
-	        <button className="search-button" type="submit">
-	          Search
-	        </button>
-    	  </form>
+		<>
+			<div className='searchBar'>
+				<form onSubmit={onSubmit} className="search-form">
+					<input className="search-bar" id='search-value' value={term} onChange={onChangeTerm} placeholder='enter your ingredients' />
+					<button className="search-button" type="submit">
+						Search
+					</button>
+				</form>
+			</div>
+			<button className="search-button clear-btn" onClick={clearText}>Clear</button>
+		</>
 	);
 }
 
