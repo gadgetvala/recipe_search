5d45020ffa9e5c15b5bdd8e50491f9a110f4390b
gadgetvala <surajj.verma7@gmail.com>
2022-05-10 20:46:47 +0530

    FEATURE: Added Loader Component

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+const Loader = () => {
+  return (
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
